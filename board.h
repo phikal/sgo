3efc0e91bfedf0f3d8c819e6b052b7d4fbc52ed1
Philip Kaludercic <philipk@posteo.net>
2021-06-26 18:30:39 +0200

    Update copyright

--- board.h
+++ board.h
@@ -1,4 +1,4 @@
-/* Copyright 2020 Philip K.
+/* Copyright 2020-2021 Philip Kaludercic
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
